b592a6478362407cdf3b5918c235c0b4f9595471
Noel Welsh <noel@untyped.com>
2022-09-28 20:12:26 +0100

    Add another explorable

--- code/src/main/scala/gradientdescent/examples/Sine.scala
+++ code/src/main/scala/gradientdescent/examples/Sine.scala
@@ -17,11 +17,13 @@
 package gradientdescent.examples
 
 import gradientdescent.utils.Plot
+import doodle.core.{Color, Point}
 import doodle.svg._
 import doodle.syntax.all._
 import doodle.explore.laminar.Explore
 import scala.scalajs.js.annotation._
 import cats.effect.unsafe.implicits.global
+import scala.util.Random
 
 @JSExportTopLevel("Sine")
 object Sine {
@@ -37,6 +39,11 @@ object Sine {
       Plot.linearInterpolation(50.0, 1.0)
     )
 
+  def dataPlot(data: Seq[Point]): Picture[Unit] =
+    data
+      .map(pt => Picture.circle(5.0).fillColor(Color.blue).noStroke.at(pt))
+      .allOn
+
   @JSExport
   def drawBasicPlot(id: String): Unit = {
     val frame = Frame(id).size(600, 200)
@@ -46,5 +53,22 @@ object Sine {
       .within(-100, 100)
       .withDefault(50)
       .explore(frame)(a => basicPlot(basicSine(a.toDouble)))
+  }
+
+  @JSExport
+  def drawErrorPlot(id: String): Unit = {
+    val trueF = basicSine(100)
+    val data = (1.to(40)).map { _ =>
+      val x = Random.between(-6.0, 6.0)
+      val y = trueF(x) + (5.0 * Random.nextGaussian())
+      Point(x * 50.0, y)
+    }
+
+    val frame = Frame(id).size(600, 200)
+    Explore
+      .int("a")
+      .within(-100, 100)
+      .withDefault(50)
+      .explore(frame)(a => basicPlot(basicSine(a.toDouble)).on(dataPlot(data)))
   }
 }
